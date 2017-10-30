baa32cefd6342487b6d8b06ac3b2ba2318293f95
Seung Hyeon Jo <chameleon001@naver.com>
2017-10-30 14:15:11 +0900

    20171030

---
+++ ImageProcessing/ImageProcessingDoc.h
@@ -0,0 +1,111 @@
+
+// ImageProcessingDoc.h : CImageProcessingDoc 클래스의 인터페이스
+//
+
+
+#pragma once
+
+
+class CImageProcessingDoc : public CDocument
+{
+protected: // serialization에서만 만들어집니다.
+	CImageProcessingDoc();
+	DECLARE_DYNCREATE(CImageProcessingDoc)
+
+// 특성입니다.
+public:
+	unsigned char* m_InputImage; //Input이미지
+	int m_width, m_height, m_size;
+
+	unsigned char* m_OutputImage; //Output이미지 추가
+	int m_Re_width, m_Re_height, m_Re_size;
+
+    double m_HIST[256];
+    double m_Sum_Of_HIST[256];
+    unsigned char m_Scale_HIST[256];
+    double** m_tempImage;
+
+// 작업입니다.
+public:
+	void OnDownSampling();
+	void OnUpSampling();
+    void OnQuantization();
+    void OnSumConstant();
+    void OnSubConstant();
+    void OnMulConstant();
+    void OnDivConstant();
+    void OnAndOperate();
+    void OnOrOperate();
+    void OnXorOperate();
+    void OnNegaTransform();
+    void OnGammaCorrection();
+    void OnBinarization();
+    void OnHistoStretch();
+    void OnHistoSpec();
+    void OnBlurr();
+    double ** OnMaskProcess55(unsigned char * Target, double Mask[5][5]);
+    double ** OnMaskProcess(unsigned char * Target, double Mask[3][3]);
+    double ** OnScale(double ** Target, int height, int width);
+    double ** Image2DMem(int height, int width);
+
+// 재정의입니다.
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// 구현입니다.
+public:
+	virtual ~CImageProcessingDoc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// 검색 처리기에 대한 검색 콘텐츠를 설정하는 도우미 함수
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+public:
+	virtual BOOL OnOpenDocument(LPCTSTR lpszPathName);
+	virtual BOOL OnSaveDocument(LPCTSTR lpszPathName);
+ //   afx_msg void OnSumConstant();
+ //   afx_msg void OnSubConstant();
+ //   afx_msg void OnMulConstant();
+ //   afx_msg void OnDivConstant();
+ //   afx_msg void OnAndOperate();
+  //  afx_msg void OnOrOperate();
+  //  afx_msg void OnXorOperate();
+  /*  afx_msg void OnNegaTransform();
+    afx_msg void OnGammaCorrection();
+    afx_msg void OnBinarization();
+    */
+    //afx_msg void OnHistoStretch();
+    afx_msg void OnEndInSearch();
+    afx_msg void OnHistogram();
+    afx_msg void OnHistoEqual();
+//    afx_msg void OnHistoSpec();
+    afx_msg void OnEmbossing();
+
+    
+     void OnGaussianFilter();
+      void OnSharpening();
+      void OnDiffOperatorHor();
+       void OnLaplacian();
+     //  double ** OnMaskProcess55(unsigned char * Target, double Mask[3][3]);
+       afx_msg void OnLog();
+       afx_msg void OnNearest();
+       afx_msg void OnBilinear();
+       afx_msg void OnMedianSub();
+       void OnBubleSort(double * A, int MAX);
+       void OnSwap(double * a, double * b);
+};
